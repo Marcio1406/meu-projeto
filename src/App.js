17b7dfddf40c50f1678e73178d46e7e36f2182fc
MarcioCarvalho <jrmarico123@mgmail.com>
2024-05-20 18:26:35 -0300

    meu-projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
+import Carousel from './components/Carousel';
+import Sobre from './components/Sobre';
+import Servicos from './components/Servicos';
+import Membros from './components/Membros';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
+        <NavBar />
+        <Carousel />
       </header>
+      <main>
+        <Sobre />
+        <Servicos />
+        <Membros />
+      </main>
+      <footer id='footer'>
+        <Footer />
+      </footer>
+      
     </div>
   );
 }
